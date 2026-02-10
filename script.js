756f948793bc7979baeb6eb0e55b09f58a6e9343
vemavarapu-daniel <71017366+vemavarapu-daniel@users.noreply.github.com>
2026-02-10 15:10:01 +0530

    Fix petal image source filename case sensitivity

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function createPetals() {
 
   setInterval(() => {
     const petal = document.createElement("img");
-    petal.src = "petal.png";
+    petal.src = "Petal.png";
     petal.className = "petal";
     petal.style.left = Math.random() * 100 + "vw";
     petal.style.animationDuration = 6 + Math.random() * 5 + "s";
